b8b0b1fbe584988d5464ec35db4ce3ae85c0c129
Jason Curl <jcurl@arcor.de>
2020-11-26 18:08:24 +0100

    CpuIdCon: Small utility to just dump the CPU register information to the current directory
    Issue: DOTNET-277

---
+++ CpuIdCon/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RJCP.Diagnostics
+{
+    using System;
+    using Intel;
+
+    public static class Program
+    {
+        public static int Main()
+        {
+            ICpuIdFactory cpuFactory = new CpuIdFactory();
+            ICpuId cpu = cpuFactory.Create();
+
+            if (cpu is GenericIntelCpuBase x86cpu) {
+                string fileName = string.Format("{0}.xml", Environment.MachineName);
+                try {
+                    x86cpu.Save(fileName);
+                    Console.WriteLine("Wrote output to: {0}", fileName);
+                    return 0;
+                } catch (Exception ex) {
+                    Console.WriteLine("Error writing to: {0}", fileName);
+                    Console.WriteLine(" -> {0}", ex.Message);
+                    return 1;
+                }
+            }
+            return 2;
+        }
+    }
+}
