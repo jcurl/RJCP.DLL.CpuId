8e18d4d22b37489cd32445d0f7c8157639e6990d
Jason Curl <jcurl@arcor.de>
2023-04-24 10:39:04 +0200

    CpuId: Fix 32-bit Native Calls
    Revert the change made in 7490e1a ("CpuId: Rework P/Invoke to use best practices") for our custom library, as on 32-bit the name of the functions is not exact, but use name mangling as per StdCall on 32-bit platforms. This would cause test cases to fail for 32-bit frameworks.
    
    On 64-bit the library (with `dumpbin /exports cpuid.dll`) shows the following, and so the test cases passed:
    
        ordinal hint RVA      name
    
              1    0 00001000 cpuid
              2    1 00001010 hascpuid
              3    2 00001030 iddump
              4    3 00001170 iddumpall
              5    4 000010C0 iddumponcore
    
    On 32-bit however, the library shows:
    
        ordinal hint RVA      name
    
              1    0 00001000 _cpuid@24
              2    1 00001010 _hascpuid@0
              3    2 00001020 _iddump@8
              4    3 00001140 _iddumpall@8
              5    4 000010D0 _iddumponcore@12
    
    and the test cases failed with the error that the entry point `hascpuid` failed.

--- CpuId/Native/CpuIdLib.cs
+++ CpuId/Native/CpuIdLib.cs
@@ -6,16 +6,16 @@
     [SuppressUnmanagedCodeSecurity]
     internal static partial class CpuIdLib
     {
-        [DllImport("cpuid.dll", ExactSpelling = true)]
+        [DllImport("cpuid.dll")]
         public static extern int hascpuid();
 
-        [DllImport("cpuid.dll", ExactSpelling = true)]
+        [DllImport("cpuid.dll")]
         public static extern int cpuid(int eax, int ecx, out int peax, out int pebx, out int pecx, out int pedx);
 
-        [DllImport("cpuid.dll", ExactSpelling = true)]
+        [DllImport("cpuid.dll")]
         public static unsafe extern int iddump(CpuIdInfo* s, int len);
 
-        [DllImport("cpuid.dll", ExactSpelling = true)]
+        [DllImport("cpuid.dll")]
         public static unsafe extern int iddumpall(CpuIdInfo* s, int len);
     }
 }
